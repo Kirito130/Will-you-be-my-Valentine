3263badfc7a350e0d34ba8d5459f6777c998e4ff
Kirito13 <66004143+Kirito130@users.noreply.github.com>
2026-02-13 04:57:41 -0500

    Delete .next directory

--- .next/static/chunks/src_app_page-3_page_tsx_f7cb7241._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_da6d75f5._.js",
-  "static/chunks/src_455a8f72._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page-6_page_tsx_f7cb7241._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_da6d75f5._.js",
-  "static/chunks/src_a2354e61._.js"
-],
-    source: "dynamic"
-});
